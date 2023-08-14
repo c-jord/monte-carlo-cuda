b457d84c3752f2430c7a332a2ecc8cf0f7089166
Jordan Contento <jordan.contento@outlook.com>
2023-08-14 03:14:40 -0400

    update gitignore and build script

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
-nvcc -shared -o build/kernels.dll .\\src\\cuda\\kernels.cu
-nvcc -o .\\build\\a.exe -L"build/" -lkernels .\\src\\main.cpp
-.\\build\\a.exe
+nvcc -shared -o build/kercppnels.dll ./src/cuda/kernels.cu
+nvcc -o ./build/a.exe -L"build/" -lkernels ./src/main.cpp
